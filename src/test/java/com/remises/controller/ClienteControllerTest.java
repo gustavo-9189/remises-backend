82b7b43b2e58d2b717cf4db52291277d60f4947e
Gustavo Martínez <gustavo-9189@hotmail.com>
2017-12-21 22:53:10 -0300

    Se agrego la clase de Test, se modifico el enum de constantes

--- src/test/java/com/remises/controller/ClienteControllerTest.java
+++ src/test/java/com/remises/controller/ClienteControllerTest.java
@@ -1,136 +1,34 @@
-//package com.remises.controller;
-// 
-//import java.util.LinkedHashMap;
-//import java.util.List;
-//
-//import org.apache.log4j.Logger;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import org.springframework.web.client.RestTemplate;
-//
-//import com.remises.global.Constantes;
-//
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(classes = {})
-//public class ClienteControllerTest {
-//	
-//	private ClienteController cliente;
-//	
-//	private static final Logger LOGGER = Logger.getLogger(ClienteController.class);
-//
-//    /* GET */
-//	@Test
-//    @SuppressWarnings("unchecked")
-//	private void listAllClientes() {
-//        LOGGER.info("Testing listAllClientes API-----------");
-//
-//        RestTemplate restTemplate = new RestTemplate();
-//        List<LinkedHashMap<String, Object>> clientesMap = restTemplate.getForObject(Constantes.URI + "cliente", List.class);
-//         
-//        if (clientesMap != null){
-//            for (LinkedHashMap<String, Object> map : clientesMap) {
-//                LOGGER.info("Cliente : id=" + map.get("id")
-//                	    + ", Viajes=" + map.get("viajes")
-//                	    + ", Nombre=" + map.get("nombre")
-//                	    + ", Apellido=" + map.get("apellido")
-//                	    + ", DNI=" + map.get("dni")
-//                	    + ", Telefono=" + map.get("telefono")
-//                	    + ", Email=" + map.get("email")
-//                	    + ", Direccion=" + map.get("direccion")
-//                	    + ", Codigo Postal=" + map.get("codigoPostal")
-//                	    + ", Ciudad=" + map.get("ciudad")
-//                	    + ", Latitud=" + map.get("latitud")
-//                	    + ", Longitud=" + map.get("longitud"));
-//            }
-//        }else{
-//            LOGGER.warn("NO EXISTEN Clientes.......");
-//        }
-//    }
-//     
-//    /* GET */
-//	@Test
-//    private static void getCliente() {
-//        LOGGER.info("Testing getCliente API----------");
-//        RestTemplate restTemplate = new RestTemplate();
-//        Cliente cliente = restTemplate.getForObject(Constantes.URI+"cliente/1", Cliente.class);
-//        LOGGER.info(cliente);
-//    }
-//
-//    /* POST */
-//	@Test
-//    private static void createCliente() {
-//        LOGGER.info("Testing createCliente API----------");
-//        RestTemplate restTemplate = new RestTemplate();
-//        Cliente cliente = new Cliente();
-//        cliente.setApellido("Martinez");
-//        cliente.setCiudad(1);
-//        cliente.setCodigoPostal(1759);
-//        cliente.setDireccion("Dragones 2505");
-//        cliente.setDni(34235783);
-//        cliente.setEmail("gustavo-9189@hotmail.com");
-//        cliente.setLatitud("123314234");
-//        cliente.setLongitud("324234233");
-//        cliente.setNombre("Gustavo Alfredo");
-//        cliente.setTelefono("2342341123");
-//        
-//        URI uri = restTemplate.postForLocation(Constantes.URI + "cliente", cliente, Cliente.class);
-//        LOGGER.info("Location : " + uri.toASCIIString());
-//    }
-// 
-//    /* PUT */
-//	@Test
-//    private static void updateCliente() {
-//        LOGGER.info("Testing updateCliente API----------");
-//        RestTemplate restTemplate = new RestTemplate();
-//        Cliente cliente  = new Cliente();
-//        cliente.setApellido("apellidoActualizado");
-//        cliente.setCiudad(2);
-//        cliente.setCodigoPostal(1111);
-//        cliente.setDireccion("direccionActualizada");
-//        cliente.setDni(40111222);
-//        cliente.setEmail("email@actualizado.com");
-//        cliente.setId(1L);
-//        cliente.setLatitud("latitudActualizada");
-//        cliente.setLongitud("longitudActualizada");
-//        cliente.setNombre("nombreActualizado");
-//        cliente.setTelefono("telefonoActualizado");
-//
-//        restTemplate.put(Constantes.URI + "cliente/1", cliente);
-//        LOGGER.info(cliente);
-//    }
-// 
-//    /* DELETE */
-//	@Test
-//    private static void deleteCliente() {
-//        LOGGER.info("Testing deleteCliente API----------");
-//        RestTemplate restTemplate = new RestTemplate();
-//        restTemplate.delete(Constantes.URI + "cliente/1");
-//    }
-// 
-// 
-//    /* DELETE */
-//	@Test
-//    private static void deleteAllClientes() {
-//        LOGGER.info("Testing all deleteAllClientes API----------");
-//        RestTemplate restTemplate = new RestTemplate();
-//        restTemplate.delete(Constantes.URI + "cliente");
-//    }
-// 
-//    // Ejecuta todas las pruebas
-//    public static void main(String args[]){
-//        listAllClientes();
-//        getCliente();
-//        createCliente();
-//        listAllClientes();
-//        updateCliente();
-//        listAllClientes();
-//        deleteCliente();
-//        createCliente();
-//        listAllClientes();
-//        deleteAllClientes();
-//        listAllClientes();
-//    }
-//
-//}
+package com.remises.controller;
+ 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.junit.Test;
+
+import com.remises.global.Constantes;
+
+public class ClienteControllerTest {
+
+	private static final String CTRL = "cliente";
+	
+	@Test
+	public void existPathNot404() throws ClientProtocolException, IOException {
+
+	   HttpUriRequest request = new HttpGet(Constantes.URI + CTRL);
+	   HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+
+	   assertThat(
+	     httpResponse.getStatusLine().getStatusCode(),
+	     equalTo(HttpStatus.SC_OK)
+	   );
+	}
+
+}
